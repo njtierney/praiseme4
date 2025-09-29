8e48e2624592b6f74bc70e71239f2f3ab915facf
njtierney <nicholas.tierney@gmail.com>
2025-09-29 12:27:31 +1000

    Add some documentation to the description field

--- R/praise.R
+++ R/praise.R
@@ -1,5 +1,7 @@
 #' @title Delivers praise to the user
 #'
+#' @description This is an example piece of documentation.
+#'
 #' @param name Name of the person you want to praise. Must be a character. Default's to the name, "Vis".
 #' @returns a nice message to user
 #' @export
